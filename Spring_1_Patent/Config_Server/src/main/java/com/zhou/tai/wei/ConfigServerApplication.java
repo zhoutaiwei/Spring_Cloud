53907904f724cffee741d93a7b8a4baece51f4d9
zhoutaiwei <1325611131@qq.com>
2017-12-18 19:36:42 +0800

    222222222

--- Spring_1_Patent/Config_Server/src/main/java/com/zhou/tai/wei/ConfigServerApplication.java
+++ Spring_1_Patent/Config_Server/src/main/java/com/zhou/tai/wei/ConfigServerApplication.java
@@ -3,7 +3,8 @@ package com.zhou.tai.wei;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@EnableEurekaClient
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
