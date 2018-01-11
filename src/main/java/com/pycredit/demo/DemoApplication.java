c9822db0b65c298318143aec054d76b6275b8521
zhoutaiwei <1325611131@qq.com>
2018-01-11 10:52:34 +0800

    添加mockmvc

---
+++ src/main/java/com/pycredit/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.pycredit.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
